b10ed8f3422025a3c4f961b87a1b831ccf3a05ae
TomiGarbe <2105905@ucc.edu.ar>
2025-04-11 20:52:16 -0300

    Sin_Pipeline

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,4 +2,4 @@ from src.api.routes import app
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000, debug=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
