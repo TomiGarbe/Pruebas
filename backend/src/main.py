d509a7314a027501c218feb000a3f9f9dc3a4deb
TomiGarbe <2105905@ucc.edu.ar>
2025-04-16 19:24:15 -0300

    Pipeline-Azure-Andando

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,16 +1,16 @@
-import os
-from dotenv import load_dotenv
-from src.api.routes import app
-
-load_dotenv('env.config')
-
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.getenv('PORT', 8000))  # Usa PORT de Azure o 8000 por defecto
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
-
+#import os
+#from dotenv import load_dotenv
 #from src.api.routes import app
+#
+#load_dotenv('env.config')
 #
 #if __name__ == "__main__":
 #    import uvicorn
-#    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+#    port = int(os.getenv('PORT', 8000))  # Usa PORT de Azure o 8000 por defecto
+#    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+
+from src.api.routes import app
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
