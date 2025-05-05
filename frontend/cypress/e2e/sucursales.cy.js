9eee158f4da2230bac775f3d7213377e7ca59b90
TomiGarbe <2105905@ucc.edu.ar>
2025-05-05 12:36:11 -0300

    Corrigiendo reportes de coverage de frontend en github actions

--- frontend/cypress/e2e/sucursales.cy.js
+++ frontend/cypress/e2e/sucursales.cy.js
@@ -5,28 +5,36 @@ describe('Gestión de Sucursales', () => {
 
   it('Carga correctamente una zona', () => {
     cy.contains('button', 'Crear Sucursal').click();
+    cy.wait(1000);
     cy.get('#zona').click();
+    cy.wait(1000);
     cy.get('.dropdown-item').click();
+    cy.wait(1000);
     cy.get('.mt-2 > #zona').type('Zona 1');
     cy.get('.mt-2 > .btn').click();
-    cy.wait(3000);
+    cy.wait(4000);
     cy.get('.btn-close').click();
+    cy.wait(1000);
   });
 
   it('Carga correctamente una sucursal', () => {
     cy.contains('button', 'Crear Sucursal').click();
     cy.get('#nombre').type('Sucursal 1');
+    cy.wait(1000);
     cy.get('#zona').click();
-    cy.wait(3000);
+    cy.wait(4000);
     cy.get('.custom-option').contains('Zona 1').click();
+    cy.wait(1000);
     cy.get('#direccion').type('Dirección 1');
     cy.get('#superficie').type('100');
+    cy.wait(1000);
     cy.get('form > .btn-primary').click();
-    cy.wait(3000);
+    cy.wait(4000);
     cy.get('tr:last-child > :nth-child(2)').should('have.text', 'NombreSucursal 1');
     cy.get('tr:last-child > :nth-child(3)').should('have.text', 'ZonaZona 1');
     cy.get('tr:last-child > :nth-child(4)').should('have.text', 'DirecciónDirección 1');
     cy.get('tr:last-child > :nth-child(5)').should('have.text', 'Superficie100');
+    cy.wait(1000);
   });
 
   it('Edita correctamente una sucursal', () => {
@@ -37,24 +45,29 @@ describe('Gestión de Sucursales', () => {
     cy.get('#direccion').type('Dirección 2');
     cy.get('#superficie').clear('100');
     cy.get('#superficie').type('200');
+    cy.wait(1000);
     cy.get('form > .btn').click();
-    cy.wait(3000);
+    cy.wait(4000);
     cy.get('tr:last-child > :nth-child(2)').should('have.text', 'NombreSucursal 2');
     cy.get('tr:last-child > :nth-child(4)').should('have.text', 'DirecciónDirección 2');
     cy.get('tr:last-child > :nth-child(5)').should('have.text', 'Superficie200');
+    cy.wait(1000);
   });
 
   it('Elimina correctamente una sucursal', () => {
     cy.get('tr:last-child > :nth-child(6) > .btn-danger').click();
+    cy.wait(1000);
     cy.get('table').find('tbody tr').should('not.exist');
+    cy.wait(1000);
   });
 
   it('Elimina correctamente una zona', () => {
     cy.contains('button', 'Crear Sucursal').click();
+    cy.wait(1000);
     cy.get('#zona').click();
-    cy.wait(3000);
+    cy.wait(4000);
     cy.get('.custom-option').contains('Zona 1').click();
-    cy.wait(3000);
+    cy.wait(4000);
     cy.get('.btn-close').click();
   });
 });
