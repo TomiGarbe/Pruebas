c82332797130f3fc974c5d628fd1e868a72c0bb0
TomiGarbe <2105905@ucc.edu.ar>
2025-06-12 14:14:44 -0300

    Prueba despliegue MVP

--- backend/src/auth/firebase.py
+++ backend/src/auth/firebase.py
@@ -1,12 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path="./env.config")
 
 def initialize_firebase():
     if not firebase_admin._apps:  # Verifica si la app ya está inicializada
         if os.getenv("TESTING") == "true":
             return None  # No inicializar Firebase en tests
-        cred = credentials.Certificate("../credentials/firebase_credentials.json")
+        cred = credentials.Certificate("/home/site/wwwroot/credentials/firebase_credentials.json")
         firebase_admin.initialize_app(cred)
     return firebase_admin.get_app()
 
