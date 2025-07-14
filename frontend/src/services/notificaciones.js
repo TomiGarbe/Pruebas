ce2556dbf75d59efd61d7b6f6f695210e1252aec
TomiGarbe <2105905@ucc.edu.ar>
2025-07-14 15:52:37 -0300

    Prueba notificaciones

--- frontend/src/services/notificaciones.js
+++ frontend/src/services/notificaciones.js
@@ -1,3 +1,3 @@
 import api from './api';
 
-export const saveToken = (token_data) => api.post('/fcm-token', token_data);
+export const saveToken = (token_data) => api.post('/fcm-token/', token_data);
