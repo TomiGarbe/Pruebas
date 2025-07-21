9b78316431cb94ccde1adf92d5de566cbd9b372c
TomiGarbe <2105905@ucc.edu.ar>
2025-07-21 13:42:08 -0300

    Prueba notificaciones

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,19 +1,15 @@
 // firebase-messaging-sw.js
+importScripts('/firebase-config.js');
 importScripts('https://www.gstatic.com/firebasejs/10.5.2/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.5.2/firebase-messaging-compat.js');
-importScripts('/firebase-config.js');
 
 firebase.initializeApp(self.FIREBASE_CONFIG);
 
 const messaging = firebase.messaging();
 
-// Prevent duplicate notifications
-messaging.onBackgroundMessage((payload) => {
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/favicon.ico',
-        tag: payload.notification.title // Use a unique tag to prevent duplicates
-    };
-    self.registration.showNotification(notificationTitle, notificationOptions);
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/favicon.ico'
+  });
 });
