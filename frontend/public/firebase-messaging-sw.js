409280e618ae1fa37976546f26de0902cb3988c6
TomiGarbe <2105905@ucc.edu.ar>
2025-07-22 15:21:38 -0300

    Prueba notificaciones

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -36,20 +36,6 @@ messaging.onBackgroundMessage((payload) => {
     }
 });
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open('pwa-cache').then(cache => {
-            return cache.addAll([
-                '/',
-                '/index.html',
-                '/favicon.ico',
-                '/icons/icon-192x192.png',
-                '/icons/icon-512x512.png'
-            ]);
-        })
-    );
-});
-
 self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request).then(response => {
