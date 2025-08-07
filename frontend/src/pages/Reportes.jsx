64d33a8d09c0e61a963115d217e99361b7b5070e
TomiGarbe <2105905@ucc.edu.ar>
2025-08-07 11:43:08 -0300

    prueba con todo

--- frontend/src/pages/Reportes.jsx
+++ frontend/src/pages/Reportes.jsx
@@ -184,7 +184,7 @@ const Reportes = () => {
     const overlapPx = 50; // cantidad de píxeles para superposición entre páginas
     const totalPages = Math.ceil((canvas.height - overlapPx) / (pageCanvasHeight - overlapPx));
 
-    let currentYOffset = 200; // primer recorte (corrige el espacio vacío arriba)
+    let currentYOffset = canvas.height * 0.035; // primer recorte (corrige el espacio vacío arriba)
 
     for (let page = 0; page < totalPages; page++) {
       const pageCanvas = document.createElement('canvas');
