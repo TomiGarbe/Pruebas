c4e3e5a410d9ecdf85b1e589122531a0a7996bdb
fgazzera <101714990+fgazzera@users.noreply.github.com>
2025-08-18 00:29:50 -0300

    fix: include vitest config to enable test options

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-      environment: 'jsdom',
-      setupFiles: './src/tests/setup.js'
-  }
+    environment: 'jsdom',
+    setupFiles: './tests/setup.ts',
+    globals: true,
+  },
 })
