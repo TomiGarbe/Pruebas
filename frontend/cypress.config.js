ddf0b728e5d1da6869c3c2bef7e0e55a6ebf1756
TomiGarbe <2105905ucc.edu.ar>
2025-08-18 14:50:02 -0300

    config cypress

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -8,7 +8,25 @@ module.exports = defineConfig({
   },
   e2e: {
     baseUrl: process.env.VITE_FRONTEND_URL,
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: "cypress/support/e2e.js",
     setupNodeEvents(on, config) {
+      const fs = require('fs');
+      const path = require('path');
+
+      on('task', {
+        readFixture(filename) {
+          const fixturePath = path.join(__dirname, 'cypress', 'fixtures', filename);
+          return fs.readFileSync(fixturePath, 'utf8');
+        },
+        resetDB() {
+          // Implement DB reset logic here
+          console.log('resetDB called');
+          return null;
+        }
+      });
+
+      return config;
     }
   },
   experimentalStudio: true
